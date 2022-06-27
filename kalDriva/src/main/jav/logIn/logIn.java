a14be5b0e66f2811cfde926db191422e1ad15de6
Conference Room <Conference Room@DESKTOP-U938QDA>
2022-06-27 18:33:15 +0000

    login

--- kalDriva/src/main/jav/logIn/logIn.java
+++ kalDriva/src/main/jav/logIn/logIn.java
@@ -18,7 +18,8 @@ import loginDao.loginDao;
 /**
  * Servlet implementation class logIn
  */
-@WebServlet(name = "logIn", value = "/logIn")
+@WebServlet("/logOut")
+//@WebServlet(name = "logIn", value = "/logIn")
 public class logIn extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
